3bfd3162932baa929a8ef02035bc7daffb10a562
KyleBishop8 <kmbishop821@gmail.com>
2025-02-12 10:11:20 -0500

    Update ThreeExamples.java

--- ThreeExamples.java
+++ ThreeExamples.java
@@ -1,5 +1,8 @@
 public class ThreeExamples {
-    public static void main (String[]args)
+    public static void main (String[]args){
 
-    System.out.println("\n\t Hello\\\");
+    System.out.println("\t\t\tDog Show Winners\n\nPlace:\t\t1\t2\t3\t4\t5\t6\nDog #:\t\t76\t32\t18\t47\t11\t29");
+    System.out.println("\tQ\"yz89\"Ja4\"tp");
+    System.out.println("\tAnderson\t3452\n\tBoyton\t\t5344\n\tCarrington\t3677\n\tHouston\t\t5988\n\tMcDougal\t5233\n\tRobertson\t3623\n\tStryker\t\t511");
+}
 }
